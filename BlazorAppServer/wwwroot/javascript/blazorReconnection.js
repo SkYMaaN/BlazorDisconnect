6869a3c98a90ceda5f3e4a7bfd08b9514a6ece65
AKO <sashakosovan4@gmail.com>
2024-09-06 23:49:52 +0300

    init

---
+++ BlazorAppServer/wwwroot/javascript/blazorReconnection.js
@@ -0,0 +1,79 @@
+﻿(() => {
+    const ReconnectionMessage = "We are having problems connecting...  Click here to reload."
+    const ReconnectedMessage = "Interactivity enabled.";
+    const maximumRetryCount = 8;
+    const retryIntervalMilliseconds = 5000;
+    //const reconnectModal = document.getElementById('reconnect-modal');
+
+    const startReconnectionProcess = () => {
+        console.log('Started reconnection process!');
+        //reconnectModal.style.display = 'block';
+
+        let isCanceled = false;
+
+        (async () => {
+            for (let i = 0; i < maximumRetryCount; i++) {
+                //reconnectModal.innerText = `Attempting to reconnect: ${i + 1} of ${maximumRetryCount}`;
+                console.log(`Attempting to reconnect: ${i + 1} of ${maximumRetryCount}`);
+
+                await new Promise(resolve => setTimeout(resolve, retryIntervalMilliseconds));
+
+                if (isCanceled) {
+                    return;
+                }
+
+                try {
+                    const result = await Blazor.reconnect();
+                    if (!result) {
+                        console.log('The server was reached, but the connection was rejected;');
+                        //location.reload();
+                        return;
+                    }
+
+                    console.log('Successfully reconnected to the server.');
+                    return;
+                } catch {
+                    console.log('Did not reach the server; trying again...');
+                }
+            }
+
+            console.log('Retried too many times; reloading the page...');
+            location.reload();
+        })();
+
+        return {
+            cancel: () => {
+                isCanceled = true;
+                //reconnectModal.style.display = 'none';
+            },
+        };
+    };
+
+    let currentReconnectionProcess = null;
+
+    Blazor.start({
+        circuit: {
+            reconnectionHandler: {
+                onConnectionDown: () => {
+                    console.log('Connection down!');
+                    createToast('Connection failed!', ReconnectionMessage);
+                    showOverlay();
+                    document.getElementById('reloadButton').style.display = 'block';
+                    currentReconnectionProcess ??= startReconnectionProcess();
+                },
+                onConnectionUp: () => {
+                    console.log('Connection up!');
+                    currentReconnectionProcess?.cancel();
+                    currentReconnectionProcess = null
+
+                    hideToasts();
+                    hideOverlay();
+                    createToast("Connection restored!", ReconnectedMessage, '3s');
+                    document.getElementById('reloadButton').style.display = 'none';
+                }
+            }
+        }
+    });
+
+    console.log('Blazor started!');
+})();
---
+++ BlazorAppServer/wwwroot/javascript/main.js
@@ -0,0 +1,49 @@
+﻿const OnOverlayClickMessage = "There is a connection problem, we are still trying to reconnect.";
+
+let toasts;
+function createToast(title, message, dismissTime) {
+    toasts.push({
+        title: title,
+        content: message,
+        dismissAfter: dismissTime
+    });
+}
+
+function hideToasts() {
+    for (let i = 0; i < toasts.stack.length; i++) {
+        toasts.closeToast(toasts.stack[i]);
+    }
+
+    var toastsInDocument = document.getElementsByClassName('toast-notification');
+    while (toastsInDocument[0]) {
+        toastsInDocument[0].parentElement.removeChild(toastsInDocument[0]);
+    }
+}
+
+function hideOverlay() {
+    var overlay = document.getElementById('overlayDiv');
+    overlay.style.display = 'none';
+}
+
+function showOverlay() {
+    var overlay = document.getElementById('overlayDiv');
+    overlay.style.display = 'block';
+}
+
+function handleOverlayClick() {
+    createToast('Wait!', OnOverlayClickMessage);
+    console.log("Overlay clicked!");
+}
+
+window.addEventListener('load', () => {
+    toasts = new Toasts({
+        offsetX: 20, // 20px
+        offsetY: 20, // 20px
+        gap: 20, // The gap size in pixels between toasts
+        width: 300, // 300px
+        timing: 'ease', // See list of available CSS transition timings
+        duration: '.5s', // Transition duration
+        dimOld: true, // Dim old notifications while the newest notification stays highlighted
+        position: 'top-left' // top-left | top-center | top-right | bottom-left | bottom-center | bottom-right
+    });
+});
